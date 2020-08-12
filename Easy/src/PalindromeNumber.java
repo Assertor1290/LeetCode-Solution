5ee0275829f8a4b70344d8afd9e54b1483f667c4
Kushagra Kumar <jakraso201@gmail.com>
2020-08-12 18:52:07 +0530

    Palidrome Number method 1

---
+++ Easy/src/PalindromeNumber.java
@@ -0,0 +1,62 @@
+/**
+ * <p>
+ * Determine whether an integer is a palindrome
+ * Could you solve it without converting the integer to a string?
+ * </p>
+ */
+public class PalindromeNumber {
+
+    /**
+     * Method 1 to determine if it is Palindrome
+     * This method converts the Integer to String
+     * Runtime: Memory
+     * @param x the integer to check if it is palindrome
+     * @return true if palindrome  or false if not a palindrome
+     */
+    public boolean isPalindrome(int x){
+        int flag=0;
+        String temp=Integer.toString(x);
+        int[] pal=new int[temp.length()];
+        for(int i=0;i<temp.length();i++){
+            //Integers 0-9 have ASCII value from 48 to 57
+            //'5' has the int value 53
+            //if we write '5'-'0' it evaluates to 53-48, or the int 5
+            pal[i]=temp.charAt(i)-'0';
+        }
+        //i starts from first index, j starts from last index
+        for(int i=0,j=pal.length-1;i<pal.length/2;i++,j--){
+            if(pal[i]!=pal[j]){
+                flag=-1;
+                break;
+            }
+        }
+        if(flag == -1)
+            return false;
+        else
+            return true;
+    }
+    /**
+     * Method 2 to determine if it is Palindrome
+     * Runtime: Memory
+     * @param x the integer to check if it is palindrome
+     * @return true if palindrome  or false if not a palindrome
+     */
+    public boolean isPalindrome2(int x){
+
+    }
+    /**
+     * Method 3 to determine if it is Palindrome
+     * Runtime: Memory
+     * @param x the integer to check if it is palindrome
+     * @return true if palindrome  or false if not a palindrome
+     */
+    public boolean isPalindrome3(int x){
+
+    }
+    public static void main(String[] args) {
+        int a=123;
+        System.out.println(new PalindromeNumber().isPalindrome(a));
+        System.out.println(new PalindromeNumber().isPalindrome2(a));
+        System.out.println(new PalindromeNumber().isPalindrome3(a));
+    }
+}

642273b15ac15221916da4382ac93a4718c3807e
Kushagra Kumar <jakraso201@gmail.com>
2020-08-15 00:20:14 +0530

    Maximum subarray sum O(n^2) solution

--- Easy/src/MaximumSubarray.java
+++ Easy/src/MaximumSubarray.java
@@ -1,6 +1,6 @@
 public class MaximumSubarray{
     /**
-     * O(n^3) solution
+     * O(n^3) solution  TLE
      * For each element, we construct all possible subarrays starting from this element.
      * Totally there are at most N^2 subarrays. Also, calculating the sum of each subarray takes O(N).
      * eg:
@@ -65,13 +65,43 @@ public class MaximumSubarray{
     }
 
     /**
+     * O(n^2) solution
+     * Problem in above code:
+     * Consider the point where we are calculating the sum:
+     *      [-2]                            =-2
+     *      [-2  1]                         =-1
+     *      [-2  1  -3]                     =-4
+     *      [-2  1  -3  4]                  = 0
+     *      [-2  1  -3  4  -1]              =-1
+     *      [-2  1  -3  4  -1  2]           = 1
+     *      [-2  1  -3  4  -1  2  1]        = 2
+     *      [-2  1  -3  4  -1  2  1  -5]    =-3
+     *      [-2  1  -3  4  -1  2  1  -5  4] = 1
      *
+     *      So to calculate sum of subarray, we are going back to extreme left and then
+     *      doing the sum from there
+     *
+     *      eg: to calculate sum of [-2 1 -3 4] we start from all the way back from -2 and go to 4
+     *      but we had already calculated the sum upto [-2 1 -3]
+     *      So in this solution, instead of going back to left, we use previous sum to calculate the new sum
+     *      i.e sum[-2 1 -3 4]= sum[-2 1 -3] + 4
      * @param nums input array
      * @return maximum subarray sum
      */
     public int maxSubArray1(int[] nums) {
 
+        int max_sum=Integer.MIN_VALUE;
+        for(int left=0;left<nums.length;left++){
+            int subarray_sum=0;
+            for(int right=left;right<nums.length;right++){
+
+                subarray_sum=subarray_sum+nums[right];
+                max_sum=Math.max(max_sum,subarray_sum);
+            }
+        }
+        return max_sum;
     }
+    
     public int maxSubArray2(int[] nums) {
 
     }

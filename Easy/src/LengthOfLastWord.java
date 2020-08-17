e8a7f334eca0af4327f2e07d141f0f1a7d0e3f45
Kushagra Kumar <jakraso201@gmail.com>
2020-08-18 04:04:20 +0530

    Length of last Word solution 2

--- Easy/src/LengthOfLastWord.java
+++ Easy/src/LengthOfLastWord.java
@@ -1,8 +1,21 @@
+/**
+ * Given a string s consists of upper/lower-case alphabets and empty space characters ' ',
+ * return the length of last word (last word means the last appearing word if we loop
+ * from left to right) in the string.
+ *
+ * If the last word does not exist, return 0.
+ */
 public class LengthOfLastWord {
+    /**
+     * Runtime:0ms  Memory:37.7MB
+     * @param s input string
+     * @return length of last word
+     */
     public int lengthOfLastWord(String s){
         int count =0;
         for(int i=s.length()-1;i>=0;i--){
             if(s.charAt(i)==' '){
+                //if it is not a trailing space, return length
                 if(count!=0) return count;
             }
             else {
@@ -12,9 +25,20 @@ public class LengthOfLastWord {
         return count;
     }
 
-    
+    /**
+     * Using pre built functions
+     * @param s input string
+     * @return length of last word
+     */
+    public int lengthOfLastWord2(String s){
+        s = s.trim();
+        int lastIndex = s.lastIndexOf(' ') + 1;
+        return s.length() - lastIndex;
+    }
+
     public static void main(String[] args) {
         String s="Hello World";
         System.out.println(new LengthOfLastWord().lengthOfLastWord(s));
+        System.out.println(new LengthOfLastWord().lengthOfLastWord2(s));
     }
 }

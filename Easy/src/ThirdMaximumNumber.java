eb2a1b8f462aa83c8c1cbb2acc3b6cba4790e019
Kushagra Kumar <jakraso201@gmail.com>
2020-08-26 01:53:17 +0530

    Third Max number using Integer class

--- Easy/src/ThirdMaximumNumber.java
+++ Easy/src/ThirdMaximumNumber.java
@@ -58,6 +58,33 @@ public class ThirdMaximumNumber {
         set.remove(set.last());
         return set.last();
     }
+
+    /**
+     * Runtime:4ms, Memory:41.4MB
+     * @param nums input array
+     * @return third maximum number
+     */
+    public int thirdMax3(int[] nums) {
+        Integer max1=null;
+        Integer max2=null;
+        Integer max3=null;
+        for(Integer n:nums){
+            if (n.equals(max1) || n.equals(max2) || n.equals(max3)) continue;
+            if(max1==null || n>max1){
+                max3=max2;
+                max2=max1;
+                max1=n;
+            }
+            else if(max2==null || n>max2){
+                max3=max2;
+                max2=n;
+            }
+            else if(max3==null || n>max3){
+                max3=n;
+            }
+        }
+        return (max3==null)?max1:max3;
+    }
     public static void main(String[] args) {
         int[] arr=new int[]{1,2};
         System.out.println(new ThirdMaximumNumber().thirdMax(arr));

5fed5e31b9b3fb318b291a8bcf38feb66285249d
Kushagra Kumar <jakraso201@gmail.com>
2020-08-29 00:12:04 +0530

    First Unique part 2

--- Easy/src/FirstUniqueCharacterInString.java
+++ Easy/src/FirstUniqueCharacterInString.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * <p>
  *     Given a string, find the first non-repeating character in it
@@ -6,6 +8,35 @@
  */
 public class FirstUniqueCharacterInString {
 
+    /**
+     * Runtime:22ms, Memory:40MB
+     * Use HashMap to store characters and their count
+     * @param s
+     * @return
+     */
+    public int firstUniqueCharacter2(String s) {
+        HashMap<Character, Integer> count = new HashMap<Character, Integer>();
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            //The getOrDefault(key, defaultValue) method of Properties class is used to get
+            //the value mapped to this key, passed as the parameter, in this Properties object.
+            //This method will fetch the corresponding value to this key, if present, and return it.
+            //If there is no such mapping, then it returns the defaultValue.
+
+            //So for first time since charcter is not there it will return 0 + 1
+            //next time it will be previous value +1
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+
+        // find the index
+        for (int i = 0; i < n; i++) {
+            if (count.get(s.charAt(i)) == 1)
+                return i;
+        }
+        return -1;
+    }
     /**
      * Runtime:1ms, Memory:40.4MB
      * @param s input string
@@ -30,5 +61,6 @@ public class FirstUniqueCharacterInString {
     public static void main(String[] args) {
         String s="loveleetcode";
         System.out.println(new FirstUniqueCharacterInString().firstUniqueCharacter(s));
+        System.out.println(new FirstUniqueCharacterInString().firstUniqueCharacter2(s));
     }
 }

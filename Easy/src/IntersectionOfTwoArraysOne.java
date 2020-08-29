7c73d32b95642f54ac687e7093df5593c79d06e2
Kushagra Kumar <jakraso201@gmail.com>
2020-08-29 23:45:16 +0530

    Intersection 2

--- Easy/src/IntersectionOfTwoArraysOne.java
+++ Easy/src/IntersectionOfTwoArraysOne.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * <p>
  *      Given two arrays, write a function to compute their intersection.
@@ -51,6 +53,54 @@ public class IntersectionOfTwoArraysOne {
         }
         return result;
     }
+    /**
+     * Runtime: 2ms, Memory:39.8MB
+     * nums1.length = n, nums2.length = m
+     * O(max(n, m) log max(n, m)) time, O(min(n, m)) space
+     * 1.Sort first array
+     * 2.Sort second array
+     * 3.Use two pointers to traverse first and second sorted arrays
+     * 4.If first pointer points to bigger value - move the second pointer
+     * 5.If second pointer points to bigger value - move the first pointer
+     * 6. If pointers point to equal values add it to result array and
+     *    move both pointers to the next distinct value
+     * 7.Repeate 4-6 until any pointer reaches the end of array
+     * 8.Shrink the size of intersection array to remove unused space
+     *
+     * @param nums1 first input array
+     * @param nums2 second input array
+     * @return intersected array
+     */
+    public int[] intersection2(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int[] intersect = new int[Math.min(nums1.length, nums2.length)];
+        int i = 0;
+        for (int p1 = 0, p2 = 0; p1 < nums1.length && p2 < nums2.length;) {
+
+            while (p1 < nums1.length && nums1[p1] < nums2[p2]) {
+                p1++;
+            }
+            while (p1 < nums1.length && p2 < nums2.length && nums1[p1] > nums2[p2]) {
+                p2++;
+            }
+            if (p1 < nums1.length && p2 < nums2.length && nums1[p1] == nums2[p2]) {
+                intersect[i++] = nums1[p1];
+                while (p1 < nums1.length && nums1[p1] == intersect[i-1]) {
+                    p1++;
+                }
+                while (p2 < nums2.length && nums2[p2] == intersect[i-1]) {
+                    p2++;
+                }
+            }
+        }
+        int[] result = new int[i];
+        for (; i-1 >= 0; i--) {
+            result[i-1] = intersect[i-1];
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
 
     }

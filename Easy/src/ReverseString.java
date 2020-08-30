040a1af2e407f71e1d0bd1178d4286095b193fd6
Kushagra Kumar <jakraso201@gmail.com>
2020-08-31 02:57:39 +0530

    Reverse String

--- Easy/src/ReverseString.java
+++ Easy/src/ReverseString.java
@@ -17,7 +17,7 @@ public class ReverseString {
     public void reverseString(char[] s) {
         char temp;
         int j=s.length-1;
-        for(int i=0;i<s.length/2;i++){
+        for(int i=0;i<j;i++){
             temp=s[i];
             s[i]=s[j];
             s[j]=temp;

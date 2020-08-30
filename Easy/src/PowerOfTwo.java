6faa11e7b99daea2f76030b6166a2eabfc3ee417
Kushagra Kumar <jakraso201@gmail.com>
2020-08-31 03:48:17 +0530

    Power of Two

--- Easy/src/PowerOfTwo.java
+++ Easy/src/PowerOfTwo.java
@@ -29,9 +29,32 @@ public class PowerOfTwo {
 
         return n>0 && Integer.bitCount(n) == 1;
     }
+    /**
+     * Runtime:2ms, Memory:38.8MB
+     * Observe the pattern
+     *               BINARY REPRESENTATION
+     *      POWER Of TWO          NUMBER ONE LESS THAN POWER OF 2
+     *      2^0 = 1  = 1                  1-1 = 0  = 0
+     *      2^1 = 2  = 10                 2-1 = 1  = 1
+     *      2^2 = 4  = 100                4-1 = 3  = 11
+     *      2^3 = 8  = 1000               8-1 = 7  = 111
+     *      2^4 = 16 = 10000             16-1 = 15 = 1111
+     *      2^5 = 32 = 100000            32-1 = 31 = 11111
+     *      2^6 = 64 = 1000000           64-1 = 63 = 111111
+     *
+     *      As we can see from pattern (n&n-1) is 0 when n is power of 2.
+     *
+     * @param n input integer
+     * @return true or false
+     */
+    public boolean isPowerOfTwo3(int n){
+
+        return n>0 && Integer.bitCount(n) == 1;
+    }
     public static void main(String[] args) {
         int n=4;
         System.out.println(new PowerOfTwo().isPowerOfTwo(n));
         System.out.println(new PowerOfTwo().isPowerOfTwo2(n));
+        System.out.println(new PowerOfTwo().isPowerOfTwo3(n));
     }
 }

7deaab391e712a9d3a3b742826c7f1c94d345002
Kushagra Kumar <jakraso201@gmail.com>
2020-09-01 18:49:01 +0530

    MissingNumber

--- Easy/src/MissingNumber.java
+++ Easy/src/MissingNumber.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * <p>
  *     Given an array containing n distinct numbers taken from 0, 1, 2, ..., n,
@@ -58,40 +62,55 @@ public class MissingNumber {
         return missing;
     }
     /**
-     * Gauss Formula
-     * Time:O(n)  Space:(1)
-     * Runtime:0ms, Memory:39.6MB
-     * This method calculates sum of array. Also sum if all elements are present= (n*n+1)/2
+     * HAshSet
+     * Time complexity : O(n)
+     * Because the set allows for O(1) containment queries, the main loop runs in
+     * O(n) time. Creating num_set costs O(n) time, as each set insertion runs in amortized
+     * O(1) time, so the overall runtime is O(n+n)=O(n).
+     *
+     * Space complexity : O(n)
+     * nums contains n-1 distinct elements, so it costs O(n) space to store a set containing all of them.
+     * Runtime:5ms, Memory:39.7MB
      * @param nums input array
      * @return missing number
      */
     public int missingNumber2(int[] nums) {
-        int sum=0;
 
-        for(int i=0;i<nums.length;i++)
-        {
-            sum+=nums[i];
-        }
-        int prod=((nums.length)*(nums.length+1))/2;
-        return prod-sum;
+            Set<Integer> set=new HashSet<>();
+            for(int i=0;i<nums.length;i++){
+                set.add(nums[i]);
+            }
+
+            int cnt=nums.length+1;
+            for(int i=0;i<cnt;i++){
+                if(!set.contains(i)) return i;
+            }
+            return -1;
     }
     /**
-     * Gauss Formula
-     * Time:O(n)  Space:(1)
-     * Runtime:0ms, Memory:39.6MB
-     * This method calculates sum of array. Also sum if all elements are present= (n*n+1)/2
+     * Brute Force
+     * Time complexity :O(nlgn)
+     * The only elements of the algorithm that have asymptotically non constant time complexity
+     * are the main for loop (which runs in O(n) time), and the sort invocation
+     * (which runs in O(nlgn) time for Python and Java). Therefore, the runtime is dominated by sort,
+     * and the entire runtime is O(nlgn).
+     *
+     * Space complexity : O(1) (or O(n))
+     * In the sample code, we sorted nums in place, allowing us to avoid allocating additional space.
+     * If modifying nums is forbidden, we can allocate an O(n) size copy and sort that instead.
+     * Runtime:7ms, Memory:48.6MB
      * @param nums input array
      * @return missing number
      */
     public int missingNumber3(int[] nums) {
-        int sum=0;
-
-        for(int i=0;i<nums.length;i++)
-        {
-            sum+=nums[i];
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=i)
+            {
+                return i;
+            }
         }
-        int prod=((nums.length)*(nums.length+1))/2;
-        return prod-sum;
+        return nums.length;
     }
 
     public static void main(String[] args) {

51da685bf15526e7a4ff613469eb834cac0f2aa4
Kushagra Kumar <jakraso201@gmail.com>
2020-09-11 19:16:32 +0530

    Median

--- Hard/src/MedianTwoSortedArrays.java
+++ Hard/src/MedianTwoSortedArrays.java
@@ -9,9 +9,9 @@ public class MedianTwoSortedArrays {
     /**
      * Iterative method
      * Runtime:2ms, Memory:40.2MB
-     * @param nums1
-     * @param nums2
-     * @return
+     * @param nums1 first input array
+     * @param nums2 second input array
+     * @return median
      */
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m=nums1.length;
@@ -57,6 +57,54 @@ public class MedianTwoSortedArrays {
             return arr[p/2];
 
         return (arr[(p/2)-1]+arr[p/2])/2.0;
+    }
+    /**
+     * Recursive method
+     * https://medium.com/@hazemu/finding-the-median-of-2-sorted-arrays-in-logarithmic-time-1d3f2ecbeb46
+     * Runtime:2ms, Memory:40.2MB
+     * @param nums1 first input array
+     * @param nums2 second input array
+     * @return median
+     */
+    public double findMedianSortedArrays2(int[] nums1, int[] nums2){
+        int m = nums1.length;
+        int n = nums2.length;
+        // to ensure m<=n // Make sure we always search the shorter array.
+        if (m > n) {
+            int[] temp = nums1; nums1 = nums2; nums2 = temp;
+            int tmp = m; m = n; n = tmp;
+        }
+        // Since A is guaranteed to be either shorter or of
+        // the same length as B, we know it can contribute
+        // 0 or all of its values to the left half of A ∪ B. => imin=0
+        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = halfLen - i;
+
+            if (i < iMax && nums2[j-1] > nums1[i]){
+                iMin = i + 1; // i is too small
+            }
+            else if (i > iMin && nums1[i-1] > nums2[j]) {
+                iMax = i - 1; // i is too big
+            }
+            else { // i is perfect
+                int maxLeft = 0;
+                if (i == 0) { maxLeft = B[j-1]; }
+                else if (j == 0) { maxLeft = A[i-1]; }
+                else { maxLeft = Math.max(A[i-1], B[j-1]); }
+                if ( (m + n) % 2 == 1 ) { return maxLeft; }
+
+                int minRight = 0;
+                if (i == m) { minRight = B[j]; }
+                else if (j == n) { minRight = A[i]; }
+                else { minRight = Math.min(B[j], A[i]); }
+
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
     }
     public static void main(String[] args) {
         int[] nums1=new int[]{1,2};

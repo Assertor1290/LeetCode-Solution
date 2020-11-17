cbb9642bea6f681ef615261586d5405b889d5187
Kushagra Kumar <jakraso201@gmail.com>
2020-11-17 14:53:23 +0530

    Search insert position 2

--- Easy/src/SearchInsertPosition.java
+++ Easy/src/SearchInsertPosition.java
@@ -7,6 +7,7 @@
  */
 public class SearchInsertPosition {
     /**
+     * O(n)
      * eg: nums:1 2 3 4 5 6, target=5
      * i=4 :nums[4]=5<5 false return 4
      *
@@ -23,9 +24,32 @@ public class SearchInsertPosition {
         }
         return i;
     }
+
+    /**
+     * Binary Search
+     * Time:O(log n) Best Case: O(1)
+     * @param nums input array
+     * @param target value to search
+     * @return index of target
+     */
+    public int searchInsert2(int[] nums,int target){
+        int l=0;
+        int r=nums.length-1;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(target<nums[mid])
+                r=mid-1;
+            else if(target>nums[mid])
+                l=mid+1;
+        }
+        return l;
+    }
     public static void main(String[] args) {
         int[] arr=new int[]{1,3,5,6};
         int target=5;
         System.out.println(new SearchInsertPosition().searchInsert(arr,target));
+        System.out.println(new SearchInsertPosition().searchInsert2(arr,target));
     }
 }

43fe6c4be2c4d0a9a67c8c7114e33c4da51fdd0d
Kushagra Kumar <jakraso201@gmail.com>
2020-11-24 01:42:44 +0530

    Memoization

--- Easy/src/ClimbingStairs.java
+++ Easy/src/ClimbingStairs.java
@@ -22,12 +22,50 @@ public class ClimbingStairs {
     }
 
     /**
-     *
+     * Recursive with Memoization
+     * TC: O(n)
+     * SC: O(n)
      * @param n steps
      * @return number of distinct ways to reach nth step.
      */
-    public int climbStairs2(int n){
+    public int climbStairs2(int n) {
+        int[] memo = new int[n + 1];
+        return climbStairsHelper(n, memo);
+    }
 
+    private int climbStairsHelper(int n, int[] memo) {
+    /*
+      0 distinct ways to climb negative steps if we
+      can only take 1 or 2 steps
+    */
+        if (n < 0) {
+            return 0;
+        }
+
+    /*
+      1 distinct way to climb if we can only take 1
+      or 2 steps.
+    */
+        if (n == 0) {
+            return 1;
+        }
+
+    /*
+      Do we already have an answer to this question (subproblem)?
+      If not fall through and compute, BUT if we already know it
+      just return it from the cache
+    */
+        if (memo[n] > 0) {
+            return memo[n];
+        }
+
+    /*
+      The answer to this subproblem is the sum of the answer to the subproblems n - 1 and n - 2
+      This drills us towards our base cases that bring us back up with an answer
+      We cache the answer before returning it so we have it later
+    */
+        memo[n] = climbStairsHelper(n - 1, memo) + climbStairsHelper(n - 2, memo);
+        return memo[n];
     }
 
     /**
@@ -41,5 +79,6 @@ public class ClimbingStairs {
 
     public static void main(String[] args) {
         System.out.println(new ClimbingStairs().climbStairs(5));
+        System.out.println(new ClimbingStairs().climbStairs2(5));
     }
 }

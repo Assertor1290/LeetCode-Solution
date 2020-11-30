ffca07c2abf98f67a94bd855934ffd5d3cde10f7
Kushagra Kumar <jakraso201@gmail.com>
2020-12-01 04:36:57 +0530

    Linked List cycle 2

--- Easy/src/LinkedListCycle.java
+++ Easy/src/LinkedListCycle.java
@@ -45,6 +45,38 @@ public class LinkedListCycle {
         }
         return false;
     }
+
+    /**
+     * Two pointers
+     * The space complexity can be reduced to O(1)O(1) by considering two pointers at different speed -
+     * a slow pointer and a fast pointer. The slow pointer moves one step at a time
+     * while the fast pointer moves two steps at a time.
+     *
+     * If there is no cycle in the list, the fast pointer will eventually reach
+     * the end and we can return false in this case.
+     *
+     * Now consider a cyclic list and imagine the slow and fast pointers are two runners
+     * racing around a circle track. The fast runner will eventually meet the slow runner.
+     * @param head of linked list
+     * @return true if it contains cycle else false
+     */
+    public boolean hasCycle2(ListNode head){
+        if (head == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         ListNode head=new ListNode(3);
         head.next=new ListNode(2);
@@ -52,5 +84,6 @@ public class LinkedListCycle {
         head.next.next.next=new ListNode(-4);
         head.next.next.next.next=head.next;
         System.out.println(new LinkedListCycle().hasCycle(head));
+        System.out.println(new LinkedListCycle().hasCycle2(head));
     }
 }
